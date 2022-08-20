9dc980a39b755609b5846753e1711b13fa4d6a1c
zaid bin abu bakar <110318992+Zaidbinabubakar@users.noreply.github.com>
2022-08-20 16:49:33 +0800

    Update 2.Pseudocode.cpp

--- Intro to C Programming Language/2.Pseudocode.cpp
+++ Intro to C Programming Language/2.Pseudocode.cpp
@@ -1,1 +1,117 @@
+/* Pseudocode (which means fake code, because its
+not really programming code) specifies the steps
+required to accomplish the task.
+• Pseudocode is a type of structured English that is
+used to specify an algorithm.
+• Pseudocode cannot be compiled nor executed, and
+there are no real formatting or syntax rules. */
 
+/*
+# Question 1
+
+Write a program that obtains two integer numbers from the
+user. It will print out the sum of those numbers.
+
+START
+Insert First Integer
+Insert Second Integer
+Calculate Sum of Two Input
+Display result output explanation
+Display result output
+END
+
+# Question 2
+
+Finding average of any three numbers.
+
+We might usually specify the procedure of solving this
+problem as “add the three numbers and divide by
+three”. Here, Read (or Ask) and Write (or Say) are
+implied. However in an algorithm, these steps have to
+be made explicit. Thus a possible algorithm is:
+
+START
+Read values of X, Y, Z
+S = X + Y + Z
+A = S / 3
+Write value of A
+END
+
+# Question 3
+
+Finding square and cube.
+
+START
+Read The value of N
+S = N * N
+C = S * N
+Write value of S, C
+END
+
+# Question 4
+
+Finding biggest of two numbers.
+
+START
+Read A, B
+If A>B, then BIG = A, otherwise BIG = B
+Write BIG
+END
+
+# Question 5
+
+Calculate pay
+
+START
+Input hours
+Input rate
+Pay = hours * rate
+Print pay
+END
+
+# Exercise :
+
+Write a Pseudocode for these problems.
+
+1. S = (A + B + C) / Y
+
+          START
+          Input = A, B, C, Y
+          S = (A + B + C)/Y
+          Print S Output
+          END
+
+2. Convert from Celsius to Fahrenheit.
+      (Multiply by 9, then divide by 5, then add 32 )
+      
+          START
+          Input = Celcius
+          Fahrenheit = (Celcius * 9)/5 + 32
+          Print Fahrenheit Output
+          END
+
+3. Area of Circle (pie r 2)
+
+          START
+          Insert = r value
+          Area_of_circle/pie = 3.14 * (r * 2) //Area of circle formula (pie r2)
+          Print Area_of_circle/pie Output
+          END
+
+4. Volume of Sphere (4/3 pie r 3)
+
+          START
+          Insert = r value
+          Area_of_circle/pie = 3.14 * (r * 3) //Volume of sphere formula (4/3 pie r3)
+          Volume_of_sphere = 4/3 * Area_of_circle/pie
+          Print Volume_of_sphere Output
+          END
+
+5. Average speed = Distance traveled/Time taken
+
+          START
+          Input = Distance traveled
+          Input = Time taken
+          Speed = Distance traveled/Time taken
+          Print Speed Output
+          END
