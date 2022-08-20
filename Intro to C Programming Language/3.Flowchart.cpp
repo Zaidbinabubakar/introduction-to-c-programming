77ac71b64d0f6da4e640d4ed2753fccbf41b23db
zaid bin abu bakar <110318992+Zaidbinabubakar@users.noreply.github.com>
2022-08-20 16:53:18 +0800

    Update 3.Flowchart.cpp

--- Intro to C Programming Language/3.Flowchart.cpp
+++ Intro to C Programming Language/3.Flowchart.cpp
@@ -1,1 +1,8 @@
+/* A flowchart is a graphical representation of an
+algorithm.
+• These flowcharts play a vital role in the programming
+of a problem and are quite helpful in understanding
+the logic of complicated and lengthy problems.
+• Once the flowchart is drawn, it becomes easy to
+write the program in any high level language. */
 
